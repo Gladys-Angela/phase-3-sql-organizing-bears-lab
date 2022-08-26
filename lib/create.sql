882f195923d8e2eddab3c1dad3df10f2f85f85a1
Gladys-Angela <gladysangelakamau@gmail.com>
2022-08-27 02:39:04 +0300

    done

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears (
+  id  INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  sex TEXT,
+  color Text,
+  temperament TEXT,
+  alive BOOLEAN
+
+);
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) 
+VALUES
+(1, "Mr.Chocolate", 4, "M", "brown", "easy", 1),
+(2, "Rowdy", 6, "M", "black", "jumpy", 1),
+(3, "Tabitha", 7, "F", "white", "cute", 1),
+(4, "Sergeant Brown", 8, "M", "brown", "difficult", 1),
+(5, "Melissa", 3, "F", "black", "easy", 0),
+(6, "Grinch", 5, "M", "white", "adaptable", 1),
+(7, "Wendy", 4, "F", "black", "fast", 0),
+(8, null, 9, "M", "green", "naive", 1)
